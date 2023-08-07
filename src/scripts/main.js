1959bc64d1ca215e0ab9c7d1cd3c7120476c2c5d
Ryzen <denisdev3loper@gmail.com>
2023-08-07 11:13:36 -0300

    contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const dataDoEvento = new Date("aug 20, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMS = 1000 * 60;
+
+    const diasproEvento = Math.floor(distanciaEvento / diaEmMs);
+    const horasProEvento = Math.floor((distanciaEvento % diaEmMs) / horaEmMs)
+    const minutosProEvento = Math.floor((distanciaEvento % horaEmMs) / minutoEmMS);
+    const segundosProEvento = Math.floor((distanciaEvento % minutoEmMS) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasproEvento}d ${horasProEvento}h ${minutosProEvento}m ${segundosProEvento}s`;
+
+    if(distanciaEvento <0 ) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado'
+    }
+},1000);
